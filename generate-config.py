f647e6bc11b194623ba0321315732ae4d4e94a9e
Zachary Juang <zachary822@me.com>
2023-06-07 22:37:10 -0400

    general improvements

--- generate-config.py
+++ generate-config.py
@@ -1,7 +1,10 @@
-from piawg import piawg
-from pick import pick
-from getpass import getpass
 from datetime import datetime
+from getpass import getpass
+
+import requests
+from pick import pick
+
+from piawg import piawg
 
 pia = piawg()
 
@@ -9,8 +12,8 @@ pia = piawg()
 pia.generate_keys()
 
 # Select region
-title = 'Please choose a region: '
-options = sorted(list(pia.server_list.keys()))
+title = "Please choose a region: "
+options = sorted(pia.server_list.keys())
 option, index = pick(options, title)
 pia.set_region(option)
 print("Selected '{}'".format(option))
@@ -19,32 +22,33 @@ print("Selected '{}'".format(option))
 while True:
     username = input("\nEnter PIA username: ")
     password = getpass()
-    if pia.get_token(username, password):
+    try:
+        pia.get_token(username, password)
         print("Login successful!")
         break
-    else:
+    except requests.HTTPError:
         print("Error logging in, please try again...")
 
 # Add key
-status, response = pia.addkey()
-if status:
-    print("Added key to server!")
-else:
-    print("Error adding key to server")
-    print(response)
+pia.addkey()
+print("Added key to server!")
 
 # Build config
 timestamp = int(datetime.now().timestamp())
-location = pia.region.replace(' ', '-')
-config_file = 'PIA-{}-{}.conf'.format(location, timestamp)
+location = pia.region.replace(" ", "-")
+config_file = "PIA-{}-{}.conf".format(location, timestamp)
 print("Saving configuration file {}".format(config_file))
-with open(config_file, 'w') as file:
-    file.write('[Interface]\n')
-    file.write('Address = {}\n'.format(pia.connection['peer_ip']))
-    file.write('PrivateKey = {}\n'.format(pia.privatekey))
-    file.write('DNS = {},{}\n\n'.format(pia.connection['dns_servers'][0], pia.connection['dns_servers'][1]))
-    file.write('[Peer]\n')
-    file.write('PublicKey = {}\n'.format(pia.connection['server_key']))
-    file.write('Endpoint = {}:1337\n'.format(pia.connection['server_ip']))
-    file.write('AllowedIPs = 0.0.0.0/0\n')
-    file.write('PersistentKeepalive = 25\n')
+with open(config_file, "w") as file:
+    file.write("[Interface]\n")
+    file.write("Address = {}\n".format(pia.connection["peer_ip"]))
+    file.write("PrivateKey = {}\n".format(pia.privatekey))
+    file.write(
+        "DNS = {},{}\n\n".format(
+            pia.connection["dns_servers"][0], pia.connection["dns_servers"][1]
+        )
+    )
+    file.write("[Peer]\n")
+    file.write("PublicKey = {}\n".format(pia.connection["server_key"]))
+    file.write("Endpoint = {}:1337\n".format(pia.connection["server_ip"]))
+    file.write("AllowedIPs = 0.0.0.0/0\n")
+    file.write("PersistentKeepalive = 25\n")
--- piawg.py
+++ piawg.py
@@ -1,13 +1,9 @@
-import requests
 import json
-from requests_toolbelt.adapters import host_header_ssl
-import urllib3
 import subprocess
 import urllib.parse
 
-# PIA uses the CN attribute for certificates they issue themselves.
-# This will be deprecated by urllib3 at some point in the future, and generates a warning (that we ignore).
-urllib3.disable_warnings(urllib3.exceptions.SubjectAltNameWarning)
+import requests
+from requests_toolbelt.adapters import host_header_ssl
 
 
 class piawg:
@@ -21,53 +17,61 @@ class piawg:
         self.connection = None
 
     def get_server_list(self):
-        r = requests.get('https://serverlist.piaservers.net/vpninfo/servers/v4')
+        r = requests.get("https://serverlist.piaservers.net/vpninfo/servers/v4")
         # Only process first line of response, there's some base64 data at the end we're ignoring
-        data = json.loads(r.text.splitlines()[0])
-        for server in data['regions']:
-            self.server_list[server['name']] = server
+        data = json.loads(r.text.partition("\n\n")[0])
+        for server in data["regions"]:
+            self.server_list[server["name"]] = server
 
     def set_region(self, region_name):
         self.region = region_name
 
     def get_token(self, username, password):
         # Get common name and IP address for metadata endpoint in region
-        meta_cn = self.server_list[self.region]['servers']['meta'][0]['cn']
-        meta_ip = self.server_list[self.region]['servers']['meta'][0]['ip']
+        meta_cn = self.server_list[self.region]["servers"]["meta"][0]["cn"]
+        meta_ip = self.server_list[self.region]["servers"]["meta"][0]["ip"]
 
         # Some tricks to verify PIA certificate, even though we're sending requests to an IP and not a proper domain
         # https://toolbelt.readthedocs.io/en/latest/adapters.html#requests_toolbelt.adapters.host_header_ssl.HostHeaderSSLAdapter
-        s = requests.Session()
-        s.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
-        s.verify = 'ca.rsa.4096.crt'
+        with requests.Session() as s:
+            s.mount("https://", host_header_ssl.HostHeaderSSLAdapter())
+            s.verify = "ca.rsa.4096.crt"
 
-        r = s.get("https://{}/authv3/generateToken".format(meta_ip), headers={"Host": meta_cn},
-                  auth=(username, password))
-        data = r.json()
-        if r.status_code == 200 and data['status'] == 'OK':
-            self.token = data['token']
-            return True
-        else:
-            return False
+            r = s.get(
+                "https://{}/authv3/generateToken".format(meta_ip),
+                headers={"Host": meta_cn},
+                auth=(username, password),
+            )
+            r.raise_for_status()
+            self.token = r.json()["token"]
 
     def generate_keys(self):
-        self.privatekey = subprocess.run(['wg', 'genkey'], stdout=subprocess.PIPE, encoding="utf-8").stdout.strip()
-        self.publickey = subprocess.run(['wg', 'pubkey'], input=self.privatekey, stdout=subprocess.PIPE,
-                                        encoding="utf-8").stdout.strip()
+        self.privatekey = subprocess.run(
+            ["wg", "genkey"], stdout=subprocess.PIPE, encoding="utf-8"
+        ).stdout.strip()
+        self.publickey = subprocess.run(
+            ["wg", "pubkey"],
+            input=self.privatekey,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        ).stdout.strip()
 
     def addkey(self):
         # Get common name and IP address for wireguard endpoint in region
-        cn = self.server_list[self.region]['servers']['wg'][0]['cn']
-        ip = self.server_list[self.region]['servers']['wg'][0]['ip']
+        cn = self.server_list[self.region]["servers"]["wg"][0]["cn"]
+        ip = self.server_list[self.region]["servers"]["wg"][0]["ip"]
 
-        s = requests.Session()
-        s.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
-        s.verify = 'ca.rsa.4096.crt'
+        with requests.Session() as s:
+            s.mount("https://", host_header_ssl.HostHeaderSSLAdapter())
+            s.verify = "ca.rsa.4096.crt"
 
-        r = s.get("https://{}:1337/addKey?pt={}&pubkey={}".format(ip, urllib.parse.quote(self.token),
-                                                                  urllib.parse.quote(self.publickey)), headers={"Host": cn})
-        if r.status_code == 200 and r.json()['status'] == 'OK':
+            r = s.get(
+                "https://{}:1337/addKey?pt={}&pubkey={}".format(
+                    ip,
+                    urllib.parse.quote(self.token),
+                    urllib.parse.quote(self.publickey),
+                ),
+                headers={"Host": cn},
+            )
+            r.raise_for_status()
             self.connection = r.json()
-            return True, r.content
-        else:
-            return False, r.content
